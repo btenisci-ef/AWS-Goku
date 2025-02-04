084a5c185b198554b882890c2e14bbe389af116d
Hoang Phi <hoang.phihuy@ef.com>
2025-02-04 15:10:30 +0100

    add

---
+++ tests/__init__.py
@@ -0,0 +1,26 @@
+
+
+# on cloud9 run pip install requests to fetch the library
+
+import requests, time
+
+url = 'https://pljqwunbh6.execute-api.eu-west-1.amazonaws.com/Prod/'
+
+obj1 = '{"Name": "Horsiana","Weight": 50}'
+obj2 = '{"Name": "Bucefalos","Weight": 65}'
+obj3 = '{"Name": "Goldilax","Weight": 45}'
+obj4 = '{"Name": "Redmalicious","Weight": 55}'
+obj5 = '{"Name": "Pegasina","Weight": 30}'
+
+
+
+x = requests.post(url, data = obj1)
+print(x)
+x = requests.post(url, data = obj2)
+print(x)
+x = requests.post(url, data = obj3)
+print(x)
+x = requests.post(url, data = obj4)
+print(x)
+x = requests.post(url, data = obj5)
+print(x)
